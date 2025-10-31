bfe8634f8a73c20c114e4849b9b37ddb24e8174c
Claryss Mae Pangasian <claryssmaep@gmail.com>
2025-10-31 22:05:05 +0800

    Update QueueManager.java

--- QueueManager.java
+++ QueueManager.java
@@ -1,7 +1,7 @@
 package PagIbig;
 public class QueueManager {
     private static QueueManager instance;
-    private int currentNumber = 0;   // starts at 0, first call returns 1
+    private int currentNumber = 0;   
 
     private QueueManager() {}
 
@@ -12,22 +12,20 @@ public class QueueManager {
         return instance;
     }
 
-    /** @return the current number (for display) */
     public synchronized int getCurrentNumber() {
         return currentNumber;
     }
 
-    /** Increment and return the next number */
     public synchronized int nextNumber() {
         currentNumber++;
         System.out.println(">>> Next queue number issued: " + currentNumber);
         return currentNumber;
     }
 
-    /** Reset to any number (used by help-desk staff) */
     public synchronized void reset(int newNumber) {
         if (newNumber < 0) newNumber = 0;
         this.currentNumber = newNumber;
         System.out.println(">>> Queue RESET to: " + currentNumber);
     }
+
 }
